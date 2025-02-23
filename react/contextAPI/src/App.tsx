565d2d2aaddef8710300fa745fb187e5642839c3
Sarfraz Alam <sarfrazk7858@gmail.com>
2025-02-23 12:16:32 +0530

    contextapi

---
+++ react/contextAPI/src/App.tsx
@@ -0,0 +1,43 @@
+import { useContext, useState } from "react"
+import { CountContext } from "./context"
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <CountContext.Provider value={count}>
+        <Count  setCount={setCount} />
+      </CountContext.Provider>
+    </div>
+  )
+}
+
+
+function Count({ setCount }: { setCount: React.Dispatch<React.SetStateAction<number>> }) {
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons setCount={setCount} />
+    </div>
+  )
+}
+
+function CountRenderer() {
+  const count = useContext(CountContext)
+  return (
+    <div>
+      {count}
+    </div>
+  )
+}
+
+function Buttons({ setCount }: { setCount: React.Dispatch<React.SetStateAction<number>> }) {
+  const count = useContext(CountContext)
+  return <div>
+    <button onClick={()=>setCount(count+1)}>incease</button>
+  </div>
+}
+
+export default App
--- react/experiments/src/App.jsx
+++ react/experiments/src/App.jsx
@@ -1,31 +1,126 @@
-import { useState } from "react";
-import "./App.css";
+// function App() {
+//   return (
+//     <div>
+//       <CardWrapper innerComponent={<TextComponent />} />
+//       <CardWrapper innerComponent={<TextComponent />} />
+//     </div>
+//   );
+// }
 
-function App() {
+import { memo, useCallback, useEffect, useMemo, useRef, useState } from "react";
+
+// function CardWrapper({ innerComponent }) {
+//   return <div>{innerComponent}</div>;
+// }
+
+// function TextComponent() {
+//   return <div>hii there</div>;
+// }
+
+// export default App;
+
+////////////////////////////////////////////////children///////////////////////////////////////
+
+// function App() {
+//   return <CardWrapper>
+//     hi there
+//     this is sarfraz
+//   </CardWrapper>;
+// }
+
+// function CardWrapper({children}) {
+//   return <div>
+//     {children}
+//   </div>;
+// }
+
+// export default App
+
+///////////////////////////////////////////////////////////////////////////////useMemo//////////////////////////////////////////////////////////////////////
+
+// function App() {
+//   const [counter, setCounter] = useState(0);
+//   const [num, setNum] = useState(1);
+
+//   let count = useMemo(() => {
+//     let x = 0;
+//     for (let i = 1; i <= num; i++) {
+//       x = x + i;
+//     }
+//     return x;
+//   }, [num]);
+
+//   // you can do something like as well
+
+//   // useEffect(() => {
+//   //   let x = 0;
+//   //   for (let i = 1; i <= num; i++) {
+//   //     x = x + i;
+//   //   }
+//   //   return x;
+//   // }, [num]);
+
+//   return (
+//     <div>
+//       <input type="number" onChange={(e) => setNum(e.target.value)} />
+//       <p>Sum is {count}</p>
+//       <button onClick={() => setCounter(counter + 1)}>
+//         Counter ({counter}){" "}
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+///////////////////////////////////////////////////////////////////////////////useCallback//////////////////////////////////////////////////////////////////////
+
+// function App() {
+//   const [counter, setCounter] = useState(0);
+//   const [num, setNum] = useState(100);
+
+//   const data = useCallback(() => {
+//     const name = "Sarrfraxz";
+//     return data;
+//   }, []);
+
+
+//   return (
+//     <div>
+//       <button onClick={() => setCounter(counter + 1)}>Click</button>
+//       <p>{counter}</p>
+
+//       <p>{num}</p>
+//       <Card />
+//     </div>
+//   );
+// }
+
+// const Card = memo(() => {
+//   return <div>Hii there</div>;
+// });
+
+// export default App;
+
+
+///////////////////////////////////////////////////////////////////////////////useRef//////////////////////////////////////////////////////////////////////
+
+function App(){
+  const divRef = useRef()
+
+  useEffect(()=>{
+    setTimeout(() => {
+       divRef.current.innerHTML="10"
+    },5000);
+  },[])
+
+  const incomeTax = 20000;
+
   return (
     <div>
-      <HeaderWithButton />
-      <Header title={"Sarfraz2"} />
-      <Header title={"Sarfraz2"} />
-      <Header title={"Sarfraz2"} />
+      hi there, your income tax is returns are <div ref={divRef}>{incomeTax}</div>
     </div>
-  );
+  )
 }
 
-function HeaderWithButton() {
-  const [status, setStatus] = useState("my name is Sarfraz");
-  function handleClick() {
-    setStatus("my name is " + Math.random());
-  }
-  return (
-    <div>
-      <button onClick={handleClick}>click</button>
-      <Header title={status} />
-    </div>
-  );
-}
-
-function Header({ title }) {
-  return <div>{title}</div>;
-}
-export default App;
+export default App
