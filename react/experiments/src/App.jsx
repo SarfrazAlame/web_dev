3c356ccd3dfbc3ef22033a33b7c9b5073765611a
Sarfraz Alam <sarfrazk7858@gmail.com>
2025-02-25 14:24:06 +0530

    code all

--- react/experiments/src/App.jsx
+++ react/experiments/src/App.jsx
@@ -1,109 +1,109 @@
-// function App() {
-//   return (
-//     <div>
-//       <CardWrapper innerComponent={<TextComponent />} />
-//       <CardWrapper innerComponent={<TextComponent />} />
-//     </div>
-//   );
-// }
+// // function App() {
+// //   return (
+// //     <div>
+// //       <CardWrapper innerComponent={<TextComponent />} />
+// //       <CardWrapper innerComponent={<TextComponent />} />
+// //     </div>
+// //   );
+// // }
 
-import { memo, useCallback, useEffect, useMemo, useRef, useState } from "react";
+// import { memo, useCallback, useEffect, useMemo, useRef, useState } from "react";
 
-// function CardWrapper({ innerComponent }) {
-//   return <div>{innerComponent}</div>;
-// }
+// // function CardWrapper({ innerComponent }) {
+// //   return <div>{innerComponent}</div>;
+// // }
 
-// function TextComponent() {
-//   return <div>hii there</div>;
-// }
+// // function TextComponent() {
+// //   return <div>hii there</div>;
+// // }
 
-// export default App;
+// // export default App;
 
-////////////////////////////////////////////////children///////////////////////////////////////
+// ////////////////////////////////////////////////children///////////////////////////////////////
 
-// function App() {
-//   return <CardWrapper>
-//     hi there
-//     this is sarfraz
-//   </CardWrapper>;
-// }
+// // function App() {
+// //   return <CardWrapper>
+// //     hi there
+// //     this is sarfraz
+// //   </CardWrapper>;
+// // }
 
-// function CardWrapper({children}) {
-//   return <div>
-//     {children}
-//   </div>;
-// }
+// // function CardWrapper({children}) {
+// //   return <div>
+// //     {children}
+// //   </div>;
+// // }
 
-// export default App
+// // export default App
 
-///////////////////////////////////////////////////////////////////////////////useMemo//////////////////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////////////////////////useMemo//////////////////////////////////////////////////////////////////////
 
-// function App() {
-//   const [counter, setCounter] = useState(0);
-//   const [num, setNum] = useState(1);
+// // function App() {
+// //   const [counter, setCounter] = useState(0);
+// //   const [num, setNum] = useState(1);
 
-//   let count = useMemo(() => {
-//     let x = 0;
-//     for (let i = 1; i <= num; i++) {
-//       x = x + i;
-//     }
-//     return x;
-//   }, [num]);
+// //   let count = useMemo(() => {
+// //     let x = 0;
+// //     for (let i = 1; i <= num; i++) {
+// //       x = x + i;
+// //     }
+// //     return x;
+// //   }, [num]);
 
-//   // you can do something like as well
+// //   // you can do something like as well
 
-//   // useEffect(() => {
-//   //   let x = 0;
-//   //   for (let i = 1; i <= num; i++) {
-//   //     x = x + i;
-//   //   }
-//   //   return x;
-//   // }, [num]);
+// //   // useEffect(() => {
+// //   //   let x = 0;
+// //   //   for (let i = 1; i <= num; i++) {
+// //   //     x = x + i;
+// //   //   }
+// //   //   return x;
+// //   // }, [num]);
 
-//   return (
-//     <div>
-//       <input type="number" onChange={(e) => setNum(e.target.value)} />
-//       <p>Sum is {count}</p>
-//       <button onClick={() => setCounter(counter + 1)}>
-//         Counter ({counter}){" "}
-//       </button>
-//     </div>
-//   );
-// }
+// //   return (
+// //     <div>
+// //       <input type="number" onChange={(e) => setNum(e.target.value)} />
+// //       <p>Sum is {count}</p>
+// //       <button onClick={() => setCounter(counter + 1)}>
+// //         Counter ({counter}){" "}
+// //       </button>
+// //     </div>
+// //   );
+// // }
 
-// export default App;
+// // export default App;
 
-///////////////////////////////////////////////////////////////////////////////useCallback//////////////////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////////////////////////useCallback//////////////////////////////////////////////////////////////////////
 
-// function App() {
-//   const [counter, setCounter] = useState(0);
-//   const [num, setNum] = useState(100);
+// // function App() {
+// //   const [counter, setCounter] = useState(0);
+// //   const [num, setNum] = useState(100);
 
-//   const data = useCallback(() => {
-//     const name = "Sarrfraxz";
-//     return data;
-//   }, []);
-
-
-//   return (
-//     <div>
-//       <button onClick={() => setCounter(counter + 1)}>Click</button>
-//       <p>{counter}</p>
-
-//       <p>{num}</p>
-//       <Card />
-//     </div>
-//   );
-// }
-
-// const Card = memo(() => {
-//   return <div>Hii there</div>;
-// });
-
-// export default App;
+// //   const data = useCallback(() => {
+// //     const name = "Sarrfraxz";
+// //     return data;
+// //   }, []);
 
 
-///////////////////////////////////////////////////////////////////////////////useRef//////////////////////////////////////////////////////////////////////
+// //   return (
+// //     <div>
+// //       <button onClick={() => setCounter(counter + 1)}>Click</button>
+// //       <p>{counter}</p>
+
+// //       <p>{num}</p>
+// //       <Card />
+// //     </div>
+// //   );
+// // }
+
+// // const Card = memo(() => {
+// //   return <div>Hii there</div>;
+// // });
+
+// // export default App;
+
+
+// ///////////////////////////////////////////////////////////////////////////////useRef//////////////////////////////////////////////////////////////////////
 
 function App(){
   const divRef = useRef()
